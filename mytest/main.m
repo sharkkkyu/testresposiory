7c1560e3ac250f6f1d790e7dde65db34f96a182e
yusongmin <yusongmin02@meituan.com>
2024-08-23 11:06:55 +0800

    Initial Commit

---
+++ mytest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mytest
+//
+//  Created by Songmin Yu on 2024/8/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
